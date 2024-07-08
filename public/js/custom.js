ec686215c01a58b4a7cb493b2a9d8eb206425988
queyangdeyangxiansheng <39573782+queyangdeyangxiansheng@users.noreply.github.com>
2024-07-08 23:21:37 +0800

    自定义js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,7 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "m9o7ghcsuu");
