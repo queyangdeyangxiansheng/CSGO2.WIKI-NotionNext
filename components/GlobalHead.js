0be6f82acc56fcbd71143b0e6b3b70cdea450aa4
queyangdeyangxiansheng <39573782+queyangdeyangxiansheng@users.noreply.github.com>
2024-07-09 00:01:10 +0800

    回滚

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -86,24 +86,6 @@ const GlobalHead = (props) => {
     `
         }}
       />
-      <script 
-      dangerouslySetInnerHTML={{
-        __html: `
-      async src="https://www.googletagmanager.com/gtag/js?id=G-V08WKYM98S"
-      `
-      }}
-      />
-      <script 
-      dangerouslySetInnerHTML={{
-        __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-V08WKYM98S');
-      `
-      }}
-      />
     </Head>
   )
 }
